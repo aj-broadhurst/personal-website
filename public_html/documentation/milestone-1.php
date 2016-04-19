a810758e9c7256330ac6059a492cb61e507336d3
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-04-19 01:16:41 -0600

    Wrote necessary documentation for milestone 1

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,5 +6,26 @@
 	</head>
 	<body>
 		<h1>Milestone 1 Documentation</h1>
+		<p><i>Personal Website Project -- Alicia Broadhurst</i></p>
+		<h2>Purpose, Audience, and Goal</h2> <!--One brief statement for each, general overview for website-->
+		<p><b>Purpose: </b>The purpose of this website is to create a portfolio, focusing not just on my web development skills, but also all my other marketable abilities. This may include writing samples (both technical and creative), other side projects, CV/resume, etc.</p>
+		<p><b>Audience: </b>The intended audience for this page would be anyone interested in employing me, or collaborating with me in a professional or semi-professional setting.</p>
+		<p><b>Goal: </b>The primary goal for this website is to establish a professional portfolio, creating a professional web presence, and a central location and gallery for everything that makes me a strong, and desirable candidate for employment or other professional work. The secondary goal would be the use of the embedded contact form in soliciting inquiries about my current availability or employment status.</p>
+		<h2>Persona</h2> <!-- Sample User -->
+		<p><b>Name</b>: Jennifer Brown<br>
+			<b>Age</b>: 35<br>
+			<b>Profession</b>: Jennifer is a Human Resources Manager at BioTek Corporation. BioTek is a small company with a focus on creating innovative, user-friendly software for biology research for use in academia, and other research environments. They are expanding and are looking to bring on more developers, including several with a focus in web development.<br>
+			<b>Technology</b>: Jennifer uses Windows 10 on a Dell Optiplex at work. Her browser of choice is Mozilla Firefox.  She has a personal iPhone 6 that she also uses to handle business matters.  At home, Jennifer uses a MacBook Pro. While she has a slight preference for MacOS, she has no strong feelings towards Windows.<br>
+			<b>Attitudes and Behaviors</b>: As Jennifer is experienced at recognizing what is needed in a candidate for any given position, she at most budgets up to 8 minutes for any given candidate. Jennifer uses all available resources to make a decision when hiring, including: resume, cover letter, portfolio, and more.  She places more emphasis on demonstrated work, as she feels seeing what candidate is capable of is very important. She also feels that seeing a portfolio also allows for more of the "personality" of a candidate to show through.<br>
+			<b>Frustrations and Needs</b>: As Jennifer is somewhat limited on time, she cannot spend a long time moving between websites in order to evaluate candidates. She prefers websites that are straightforward in layout, and that present the very best highlights of a candidates work without becoming too cluttered, or clogged up with information.<br>
+			<b>Goals</b>: Jennifer is looking to hire well-rounded candidates with demonstrated experience.  She is also trying to get a feel for the candidate to see if they would fit in with the culture at BioTek.  If all her questions are answered satisfactorily, and she has a good feeling about a potential candidate, then she will follow up about scheduling a phone or in-person interview.<br>
+		<h2>Use Case</h2>
+		<ol start="0">
+			<li>Jennifer navigates to the landing page at <a href = "https://ajbroadhurst.com">ajbroadhurst.com</a> as part of my application for one of the Junior Web Developer Positions.</li>
+			<li>Reads the brief 'About Me' summary blurb welcoming users upon their arrival to the landing page before choosing to look at the other page options.</li>
+			<li>From 'Portfolio', 'About Me', 'Extra Credit', 'Contact', Jennifer chooses 'Portfolio'.</li>
+			<li>Jennifer reviews the material on the 'Portfolio' page.</li>
+			<li>She glances quickly over the 'About Me' page, skims my resume/LinkedIn once more, and decides to follow up and schedule a phone interview.</li>
+		</ol>
 	</body>
 </html>
