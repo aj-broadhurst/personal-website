7d11b52cd4dc94f05c0ffc12da13df6a6145d84b
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-05-02 01:54:33 -0600

    Added SVGs for wireframes and flowchart.  Added required fields for milestone 2.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -5,7 +5,44 @@
 		<title>Milestone 2</title>
 	</head>
 	<body>
-
-		
+		<main>
+			<h1>Milestone 2</h1>
+			<p><i>Personal Website Project -- Alicia Broadhurst</i></p>
+			<h2>Sitemap</h2> <!--Listing of Site Pages-->
+			<p>The site will consist of the following pages.  All are top-level, there are no subpages for the minimum viable product. <br>
+				<strong>Pages:</strong></p>
+				<ul>
+					<li>Main Menu (Homepage)</li>
+					<li>Stats (About Me)</li>
+					<li>Adventures (Portfolio)</li>
+				</ul>
+			<h2>Content Strategy</h2>
+				<p><strong>Main Menu: </strong>Landing page for the entire website, first impression, and primary source for introducing direction into navigation of the website as a whole. Provides a brief blurb before directing users towards the 'Stats', or 'Adventures' pages for more information.</p>
+				<p><strong>Stats: </strong>Contains the typical information you would expect from an About Me page--an overview of past education, professional history, skillbase, and other miscellanea. The ability to download a full resume is provided here.  Also contains the contact form for those interested in me professionally, or those who would like more information about myself or past projects. Provides links for professional social networking pages, like LinkedIn, and GitHub, though those are linked as icons in the sidebar as well.  The overall tone should be personable and relaxed but not overly colloquial. </p>
+				<p><strong>Adventures: </strong> This page contains a gallery of professional and personal projects that are worth highlighting.  It utilizes an interactive slideshow format in order to maximize impact.  Each project will feature an image and then a short project description, including links to external webpages, or other downloadable project material. </p>
+			<h2>Interaction Flow</h2> <!--Insert Flowchart SVG here-->
+			<img src="images/personal-website-flow.svg" alt="Interaction Flowchart" height="40%" width="40%">
+			<ol start="0">
+				<li>Jennifer Brown navigates to the landing page at <a href = "https://ajbroadhurst.com">ajbroadhurst.com</a>.</li>
+				<li>Reads the brief text hook welcoming users, as well as catching their interest upon their arrival to the landing page.  Once finished, she chooses from the other page options.</li>
+				<li>Jennifer chooses 'Adventures'.</li>
+				<li>Jennifer reviews the material on the 'Adventures' page.</li>
+				<li>Once finished, she navigates to the 'Stats' page.</li>
+				<li>She glances quickly over the 'Stats' page; once finished she utilizes the contact form at the very bottom of the page.</li>
+			</ol>
+			<h2>Wireframes</h2> <!--Wireframe SVGs here below-->
+			<p>Wireframes are as follows in order: Desktop Main Content, Mobile Main Content, Desktop Portfolio, Mobile Portfolio.</p>
+			<h4>Main Content Wireframe - Desktop</h4>
+			<img src="images/pwp-content-wireframe-desktop.svg" alt="Desktop Main Content Wireframe">
+			<br>
+			<h4>Main Content Wireframe - Mobile</h4>
+			<img src="images/pwp-content-wireframe-mobile.svg" alt="Mobile Main Content Wireframe">
+			<br>
+			<h4>Portfolio Wireframe - Desktop</h4>
+			<img src="images/pwp-portfolio-wireframe-desktop.svg" alt="Desktop Portfolio Wireframe">
+			<br>
+			<h4>Portfolio Wireframe - Mobile</h4>
+			<img src="images/pwp-portfolio-wireframe-mobile.svg" alt="Mobile Portfolio Wireframe">
+		</main>
 	</body>
 </html>
