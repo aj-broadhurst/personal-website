c1c097b180d6f8db2d633c1eea4ed677751db5e0
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-05-31 05:00:42 -0600

    messing around with sidebars

--- index.php
+++ index.php
@@ -17,7 +17,165 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
 
 		<!-- Custom CSS -->
-		<link rel="stylesheet" href="style.css" type="text/css">
+		<link rel="stylesheet" href="style.css" type="text/css"><!DOCTYPE html>
+		<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+				<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+				<!-- Latest compiled and minified CSS -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+						integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+				<!-- Optional theme -->
+				<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+						integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r"
+						crossorigin="anonymous"/>
+
+				<!-- Font Awesome Icons -->
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
+
+				<!-- Custom CSS -->
+				<link rel="stylesheet" href="style.css" type="text/css">
+				<link rel="stylesheet" href="sidebar.css" type="text/css">
+
+				<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+				<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+				<!--[if lt IE 9]>
+				<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+				<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+				<![endif]-->
+
+				<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+				<script src="https://code.jquery.com/jquery-2.2.3.min.js"
+						  integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
+
+
+				<!-- Latest compiled and minified JavaScript -->
+				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+						  integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+						  crossorigin="anonymous"></script>
+
+				<!-- own JavaScript -->
+				<script src="sidebar.js"></script>
+
+
+				<title>Stats</title>
+			</head>
+
+	<body class="sfooter">
+		<div id="wrapper">
+			<div class="overlay"></div>
+			<!-- Sidebar -->
+			<nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+				<ul class="nav sidebar-nav">
+					<li class="sidebar-brand">
+						<a href="#">
+							Logo
+						</a>
+					</li>
+					<li>
+						<a href="#">Main Menu</a>
+					</li>
+					<li>
+						<a href="#">Stats</a>
+					</li>
+					<li>
+						<a href="#">Adventures</a>
+					</li>
+				</ul>
+			</nav>
+			<!-- /#sidebar-wrapper -->
+
+			<!-- Page Content -->
+			<div id="page-content-wrapper">
+				<button type="button" class="hamburger is-closed" data-toggle="offcanvas">
+					<span class="hamb-top"></span>
+					<span class="hamb-middle"></span>
+					<span class="hamb-bottom"></span>
+				</button>
+				<div class="sfooter-content">
+					<header>
+
+					</header>
+					<main>
+						<section id="Title">
+							<div class="container">
+								<div class="row">
+									<div class="col-xs-12">
+										<div class="jumbotron text-center">
+											<h1>Title Banner</h1>
+											<p>Welcome Text</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+						<section id="Hook">
+							<div class="container">
+								<div class="row">
+									<div class="col-xs-12">
+										<div class="well text-center">
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum faucibus
+												diam, a sodales dolor volutpat molestie. Aliquam mattis lorem bibendum interdum
+												maximus. Nullam cursus urna mauris, semper ultricies nulla accumsan at. Sed id
+												commodo urna, nec placerat elit. In sem sapien, faucibus convallis neque a,
+												sollicitudin scelerisque nulla. Mauris lobortis nulla mattis, euismod lorem id,
+												viverra diam. Integer non dui ac ipsum ultricies euismod. Mauris id quam metus.
+												Maecenas a scelerisque magna. Nam quis quam sit amet dui facilisis rhoncus sed non
+												purus. Aenean euismod maximus nibh sit amet semper. </p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+						<section id="Content">
+							<div class="container">
+								<div class="row">
+									<div class="col-md-6">
+										<div class="well">
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum faucibus
+												diam, a sodales dolor volutpat molestie. Aliquam mattis lorem bibendum interdum
+												maximus. Nullam cursus urna mauris, semper ultricies nulla accumsan at. Sed id
+												commodo urna, nec placerat elit. In sem sapien, faucibus convallis neque a,
+												sollicitudin scelerisque nulla. Mauris lobortis nulla mattis, euismod lorem id,
+												viverra diam. Integer non dui ac ipsum ultricies euismod. Mauris id quam metus.
+												Maecenas a scelerisque magna. Nam quis quam sit amet dui facilisis rhoncus sed non
+												purus. Aenean euismod maximus nibh sit amet semper.</p>
+										</div>
+									</div>
+									<div class="col-md-6">
+										<div class="well">
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum faucibus
+												diam, a sodales dolor volutpat molestie. Aliquam mattis lorem bibendum interdum
+												maximus. Nullam cursus urna mauris, semper ultricies nulla accumsan at. Sed id
+												commodo urna, nec placerat elit. In sem sapien, faucibus convallis neque a,
+												sollicitudin scelerisque nulla. Mauris lobortis nulla mattis, euismod lorem id,
+												viverra diam. Integer non dui ac ipsum ultricies euismod. Mauris id quam metus.
+												Maecenas a scelerisque magna. Nam quis quam sit amet dui facilisis rhoncus sed non
+												purus. Aenean euismod maximus nibh sit amet semper. </p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+
+					</main>
+				</div>
+				<footer>
+					<div class="container">
+						<div class="copyright text-center">
+							&copy; AJ Broadhurst 2016
+						</div>
+					</div>
+				</footer>
+			</div>
+			<!-- /#page-content-wrapper -->
+		</div>
+
+	</body>
+</html>
 
 		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
--- stats.php
+++ stats.php
@@ -6,7 +6,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+				integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
@@ -18,6 +19,7 @@
 
 		<!-- Custom CSS -->
 		<link rel="stylesheet" href="style.css" type="text/css">
+		<link rel="stylesheet" href="sidebar.css" type="text/css">
 
 		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -27,70 +29,131 @@
 		<![endif]-->
 
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="   crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-2.2.3.min.js"
+				  integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
 
 
 		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+				  integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+				  crossorigin="anonymous"></script>
+
+		<!-- own JavaScript -->
+		<script src="sidebar.js"></script>
 
 
-		<title>Personal Website</title>
+		<title>Stats</title>
 	</head>
 
 	<body class="sfooter">
-		<div class="sfooter-content">
-			<header>
+		<div id="wrapper">
+			<div class="overlay"></div>
+			<!-- Sidebar -->
+			<nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+				<ul class="nav sidebar-nav">
+					<li class="sidebar-brand">
+						<a href="#">
+							Logo
+						</a>
+					</li>
+					<li>
+						<a href="#">Main Menu</a>
+					</li>
+					<li>
+						<a href="#">Stats</a>
+					</li>
+					<li>
+						<a href="#">Adventures</a>
+					</li>
+				</ul>
+			</nav>
+			<!-- /#sidebar-wrapper -->
 
-			</header>
-			<main>
-				<section id="Title">
-					<div class="container">
-						<div class="row">
-							<div class="col-xs-12">
-								<div class="jumbotron text-center">
-									<h1>Stats</h1>
-									<p>Welcome Text</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</section>
-				<section id="Hook">
-					<div class="container">
-						<div class="row">
-							<div class="col-xs-12">
-								<div class="well text-center">
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum faucibus diam, a sodales dolor volutpat molestie. Aliquam mattis lorem bibendum interdum maximus. Nullam cursus urna mauris, semper ultricies nulla accumsan at. Sed id commodo urna, nec placerat elit. In sem sapien, faucibus convallis neque a, sollicitudin scelerisque nulla. Mauris lobortis nulla mattis, euismod lorem id, viverra diam. Integer non dui ac ipsum ultricies euismod. Mauris id quam metus. Maecenas a scelerisque magna. Nam quis quam sit amet dui facilisis rhoncus sed non purus. Aenean euismod maximus nibh sit amet semper. </p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</section>
-				<section id="Content">
-					<div class="container">
-						<div class="row">
-							<div class="col-md-6">
-								<div class="well">
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum faucibus diam, a sodales dolor volutpat molestie. Aliquam mattis lorem bibendum interdum maximus. Nullam cursus urna mauris, semper ultricies nulla accumsan at. Sed id commodo urna, nec placerat elit. In sem sapien, faucibus convallis neque a, sollicitudin scelerisque nulla. Mauris lobortis nulla mattis, euismod lorem id, viverra diam. Integer non dui ac ipsum ultricies euismod. Mauris id quam metus. Maecenas a scelerisque magna. Nam quis quam sit amet dui facilisis rhoncus sed non purus. Aenean euismod maximus nibh sit amet semper.</p>
-								</div>
-							</div>
-							<div class="col-md-6">
-								<div class="well">
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum faucibus diam, a sodales dolor volutpat molestie. Aliquam mattis lorem bibendum interdum maximus. Nullam cursus urna mauris, semper ultricies nulla accumsan at. Sed id commodo urna, nec placerat elit. In sem sapien, faucibus convallis neque a, sollicitudin scelerisque nulla. Mauris lobortis nulla mattis, euismod lorem id, viverra diam. Integer non dui ac ipsum ultricies euismod. Mauris id quam metus. Maecenas a scelerisque magna. Nam quis quam sit amet dui facilisis rhoncus sed non purus. Aenean euismod maximus nibh sit amet semper. </p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</section>
+			<!-- Page Content -->
+			<div id="page-content-wrapper">
+				<button type="button" class="hamburger is-closed" data-toggle="offcanvas">
+					<span class="hamb-top"></span>
+					<span class="hamb-middle"></span>
+					<span class="hamb-bottom"></span>
+				</button>
+				<div class="sfooter-content">
+					<header>
 
-			</main>
-		</div>
-		<footer>
-			<div class="container">
-				<div class="copyright text-center">
-					&copy; AJ Broadhurst 2016
+					</header>
+					<main>
+						<section id="Title">
+							<div class="container">
+								<div class="row">
+									<div class="col-xs-12">
+										<div class="jumbotron text-center">
+											<h1>Title Banner</h1>
+											<p>Welcome Text</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+						<section id="Hook">
+							<div class="container">
+								<div class="row">
+									<div class="col-xs-12">
+										<div class="well text-center">
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum faucibus
+												diam, a sodales dolor volutpat molestie. Aliquam mattis lorem bibendum interdum
+												maximus. Nullam cursus urna mauris, semper ultricies nulla accumsan at. Sed id
+												commodo urna, nec placerat elit. In sem sapien, faucibus convallis neque a,
+												sollicitudin scelerisque nulla. Mauris lobortis nulla mattis, euismod lorem id,
+												viverra diam. Integer non dui ac ipsum ultricies euismod. Mauris id quam metus.
+												Maecenas a scelerisque magna. Nam quis quam sit amet dui facilisis rhoncus sed non
+												purus. Aenean euismod maximus nibh sit amet semper. </p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+						<section id="Content">
+							<div class="container">
+								<div class="row">
+									<div class="col-md-6">
+										<div class="well">
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum faucibus
+												diam, a sodales dolor volutpat molestie. Aliquam mattis lorem bibendum interdum
+												maximus. Nullam cursus urna mauris, semper ultricies nulla accumsan at. Sed id
+												commodo urna, nec placerat elit. In sem sapien, faucibus convallis neque a,
+												sollicitudin scelerisque nulla. Mauris lobortis nulla mattis, euismod lorem id,
+												viverra diam. Integer non dui ac ipsum ultricies euismod. Mauris id quam metus.
+												Maecenas a scelerisque magna. Nam quis quam sit amet dui facilisis rhoncus sed non
+												purus. Aenean euismod maximus nibh sit amet semper.</p>
+										</div>
+									</div>
+									<div class="col-md-6">
+										<div class="well">
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum faucibus
+												diam, a sodales dolor volutpat molestie. Aliquam mattis lorem bibendum interdum
+												maximus. Nullam cursus urna mauris, semper ultricies nulla accumsan at. Sed id
+												commodo urna, nec placerat elit. In sem sapien, faucibus convallis neque a,
+												sollicitudin scelerisque nulla. Mauris lobortis nulla mattis, euismod lorem id,
+												viverra diam. Integer non dui ac ipsum ultricies euismod. Mauris id quam metus.
+												Maecenas a scelerisque magna. Nam quis quam sit amet dui facilisis rhoncus sed non
+												purus. Aenean euismod maximus nibh sit amet semper. </p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+
+					</main>
 				</div>
+				<footer>
+					<div class="container">
+						<div class="copyright text-center">
+							&copy; AJ Broadhurst 2016
+						</div>
+					</div>
+				</footer>
 			</div>
-		</footer>
+			<!-- /#page-content-wrapper -->
+		</div>
+
 	</body>
 </html>
